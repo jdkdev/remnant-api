c784681e846b4b789da48875388175bc7908b40b
Jordan <knightworks@pm.me>
2020-03-18 20:26:02 -0500

    feat | locked down router

--- src/Server/router.js
+++ src/Server/router.js
@@ -12,51 +12,34 @@ router.use('/', (req, res, next) => {
 })
 
 let UserController = require('$c/UserController')
-let AuthController = require('$c/AuthController')
 let BibleController = require('$c/BibleController')
 
-router.get(
-  '/test',
-  AuthController.authenticateTokenMiddleware,
-  BibleController.test
-)
-router.get(
-  '/study',
-  AuthController.authenticateTokenMiddleware,
-  BibleController.index
-)
+let AuthController = require('$c/AuthController')
+let atm = AuthController.authenticateTokenMiddleware
+
+router.get('/test', atm, BibleController.test)
+router.get('/study', atm, BibleController.index)
 // router.get('/study/:book_id', BibleController.show)
-router.get('/books/indices', BibleController.indices)
-router.get('/books/:book_id', BibleController.show)
-router.get(
-  '/verses/:id',
-  AuthController.authenticateTokenMiddleware,
-  BibleController.verse
-)
-router.get('/search/:searchTerm', BibleController.search)
+router.get('/books/indices', atm, BibleController.indices)
+router.get('/books/:book_id', atm, BibleController.show)
+router.get('/verses/:id', atm, BibleController.verse)
+router.get('/search/:searchTerm', atm, BibleController.search)
 
-router.get(
-  '/users',
-  AuthController.authenticateTokenMiddleware,
-  UserController.index
-)
-router.get(
-  '/users/all',
-  AuthController.authenticateTokenMiddleware,
-  UserController.all
-)
-router.post('/users', UserController.store)
-router.delete('/users/:id', UserController.destroy)
-router.patch('/users/:id/restore', UserController.restore)
+router.get('/users', atm, UserController.index)
+router.get('/users/all', atm, UserController.all)
 
-router.post('/register', AuthController.register)
-router.post('/login', AuthController.login)
-router.post('/refresh', AuthController.refresh)
-router.get(
-  '/verify',
-  AuthController.authenticateTokenMiddleware,
-  AuthController.verify
-)
-router.post('/logout', AuthController.logout)
+// router.post('/users', UserController.store)
+// router.delete('/users/:id', UserController.destroy)
+// router.patch('/users/:id/restore', UserController.restore)
+
+// router.post('/register', AuthController.register)
+// router.post('/login', AuthController.login)
+// router.post('/refresh', AuthController.refresh)
+// router.get(
+//   '/verify',
+//   AuthController.authenticateTokenMiddleware,
+//   AuthController.verify
+// )
+// router.post('/logout', AuthController.logout)
 
 module.exports = router
